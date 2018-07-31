28e2686cba1ce5fc871219b38907673a065706f7
david balick <davidbalick@gmail.com>
2018-07-31 11:16:28 -0400

    Added Elapsed Time display code

--- UiLibrary.cs
+++ UiLibrary.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace WellNet.Utils
 {
@@ -43,6 +45,59 @@ namespace WellNet.Utils
             var nameWoPath = string.Format("{0}{1}", nameWoExt, ext);
             var newName = Path.Combine(dir, nameWoPath);
             File.Move(fileName, newName);
+        }
+
+        public static string TimeElapsed(TimeSpan timeSpan, bool showMilliseconds)
+        {
+            var list = new List<string>();
+            var timeParts = new string[(showMilliseconds) ? 5 : 4];
+            timeParts[0] = "day";
+            timeParts[1] = "hour";
+            timeParts[2] = "minute";
+            timeParts[3] = "second";
+            if (showMilliseconds) timeParts[4] = "millisecond";
+            for (var t = 0; t < timeParts.Length; t++)
+            {
+                var text = ElapsedPart(timeParts[t], GetTimeValue(t, timeSpan));
+                if (text != null) list.Add(text);
+            }
+            for (var t = 0; t < list.Count - 2; t++) list[t] = string.Format("{0}, ", list[t]);
+            if (list.Count > 1) list.Insert(list.Count - 1, " and ");
+            var sb = new StringBuilder();
+            for (var t = 0; t < list.Count; t++) sb.Append(list[t]);
+            return sb.ToString();
+        }
+
+        private static int GetTimeValue(int timePart, TimeSpan timeSpan)
+        {
+            var result = 0;
+            switch (timePart)
+            {
+                case 0:
+                    result = timeSpan.Days;
+                    break;
+                case 1:
+                    result = timeSpan.Hours;
+                    break;
+                case 2:
+                    result = timeSpan.Minutes;
+                    break;
+                case 3:
+                    result = timeSpan.Seconds;
+                    break;
+                case 4:
+                    result = timeSpan.Milliseconds;
+                    break;
+            }
+            return result;
+        }
+
+        private static string ElapsedPart(string partName, int numberOf)
+        {
+            string result = null;
+            if (numberOf > 0)
+                result = string.Format("{0} {1}{2}", numberOf, partName, (numberOf != 1) ? "s" : "");
+            return result;
         }
     }
 }
