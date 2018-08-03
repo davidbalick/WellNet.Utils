7078a9ba183676a945894a3d9035340a1b04aca5
david balick <davidbalick@gmail.com>
2018-08-02 21:51:06 -0400

    Added ViewModelBase

---
+++ ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace WellNet.Utils
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Propertys
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                if (value.Equals(_status))
+                    return;
+                _status = value;
+                OnPropertyChanged("Status");
+            }
+        }
+        #endregion Propertys
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
